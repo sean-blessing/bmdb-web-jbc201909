fbafe2b541f5e800e31b36d9d319e3fa31215d41
Sean Blessing <snblessing@gmail.com>
2019-11-06 16:07:57 -0500

    init commit

---
+++ src/main/java/com/bmdb/web/MovieController.java
@@ -0,0 +1,107 @@
+package com.bmdb.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.*;
+
+import com.bmdb.business.Movie;
+import com.bmdb.db.MovieRepository;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+	@Autowired
+	private MovieRepository movieRepo;
+	
+	// list - return all movies
+	@GetMapping("/")
+	public JsonResponse listStuffies() {
+		JsonResponse jr = null;
+		try  {
+			jr = JsonResponse.getInstance(movieRepo.findAll());			
+		}
+		catch (Exception e) {
+			jr = JsonResponse.getInstance(e.getMessage());
+		}
+		return jr;
+	}
+	
+	// get - return 1 stuff for the given id
+	@GetMapping("/{id}")
+	public JsonResponse getMovie(@PathVariable int id) {
+		JsonResponse jr = null;
+		try {
+			jr = JsonResponse.getInstance(movieRepo.findById(id));
+		}
+		catch (Exception e) {
+			jr = JsonResponse.getInstance(e.getMessage());
+		}
+		return jr;
+	}
+	
+	// add - adds a new Movie
+	@PostMapping("/")
+	public JsonResponse addMovie(@RequestBody Movie m) {
+		// add a new movie
+		JsonResponse jr = null;
+		try {
+			jr = JsonResponse.getInstance(movieRepo.save(m));
+		}
+		catch (DataIntegrityViolationException dive) {
+			jr = JsonResponse.getInstance(dive.getRootCause().getMessage());
+		}
+		catch (Exception e) {
+			jr = JsonResponse.getInstance(e.getMessage());
+		}
+		return jr;
+	}
+	
+	// update - update a Movie
+	@PutMapping("/")
+	public JsonResponse updateMovie(@RequestBody Movie m) {
+		// update a movie
+		JsonResponse jr = null;
+		try {
+			if (movieRepo.existsById(m.getId())) {
+				jr = JsonResponse.getInstance(movieRepo.save(m));
+			}
+			else {
+				// record doesn't exist
+				jr = JsonResponse.getInstance("Error updating Movie.  id: "+
+											m.getId() + " doesn't exist!");
+			}
+		}
+		catch (Exception e) {
+			jr = JsonResponse.getInstance(e.getMessage());
+		}
+		return jr;
+	}
+	
+	// delete - delete a Movie
+	@DeleteMapping("/{id}")
+	public JsonResponse deleteMovie(@PathVariable int id) {
+		// delete a movie
+		JsonResponse jr = null;
+		
+		try {
+			if (movieRepo.existsById(id)) {
+				movieRepo.deleteById(id);
+				jr = JsonResponse.getInstance("Delete successful!");
+			}
+			else {
+				// record doesn't exist
+				jr = JsonResponse.getInstance("Error deleting Movie.  id: "+
+											id + " doesn't exist!");
+			}
+		}
+		catch (DataIntegrityViolationException dive) {
+			jr = JsonResponse.getInstance(dive.getRootCause().getMessage());
+		}
+		catch (Exception e) {
+			jr = JsonResponse.getInstance(e.getMessage());
+		}
+		return jr;
+	}
+
+}
